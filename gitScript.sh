b52b1f5e37b242681a0dc44ca02bcb15605f27d2
HugoSilva92 <hugo.f.o.silva@gmail.com>
2023-08-25 11:42:07 +0100

    Script for pulling or cloning all repos

--- gitScript.sh
+++ gitScript.sh
@@ -3,27 +3,22 @@
 # Your GitHub username
 USERNAME="HugoSilva92"
 
-# Directory where you want to store the repositories
-
-# Create the directory if it doesn't exist
-
 # Get a list of your repositories using the GitHub API
 REPOS_JSON=$(curl -s "https://api.github.com/users/$USERNAME/repos")
 
 # Loop through the repositories and clone or pull
 for repo in $(echo "$REPOS_JSON" | jq -r '.[].ssh_url'); do
     repo_name=$(basename $repo .git)
-    repo_path="/$repo_name"
     
-    if [ -d "$repo_path" ]; then
+    if [ -d "$repo_name" ]; then
         # If the repository exists locally, pull the latest changes
         echo "Pulling $repo_name..."
-        cd "$repo_path"
+        cd "$repo_name"
         git pull
         cd -
     else
         # If the repository doesn't exist locally, clone it
-        echo "Cloning $repo..."
+        echo "Cloning $repo_name..."
         git clone $repo
     fi
 done
